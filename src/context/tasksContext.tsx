a10b8dbc82aafeb669821fa8c1fcfbe712aae6ff
Cassio <cassioate@gmail.com>
2022-08-05 01:47:00 -0300

    refactor: changed to the ContextAPI instead of using props

---
+++ src/context/tasksContext.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export interface ITasksContext {
+  id: number;
+  title: string;
+}
+
+export const TasksContext = React.createContext<ITasksContext>(
+  {} as ITasksContext,
+);
