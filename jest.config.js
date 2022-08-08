b882019b026c65a76b69a5173c770bc866c79cb7
Cassio <cassioate@gmail.com>
2022-08-08 12:01:50 -0300

    chore: configured the jest config with coverage

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,24 @@
 module.exports = {
-  "preset": "react-native",
-  "setupFilesAfterEnv": [
+  preset: "react-native",
+  setupFilesAfterEnv: [
     "@testing-library/jest-native/extend-expect"
   ],
-  "testPathIgnorePatterns": ["node_modules", "/android", "/ios"],
-  "moduleFileExtensions": [
+  testPathIgnorePatterns: ["node_modules", "/android", "/ios", "/coverage"],
+  moduleFileExtensions: [
     "ts",
     "tsx",
     "js",
     "jsx",
     "json",
     "node"
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.spec.tsx",
+    "!src/**/*.test.tsx",
+  ],
+  coverageReporters: [
+    "lcov",
   ]
 }
